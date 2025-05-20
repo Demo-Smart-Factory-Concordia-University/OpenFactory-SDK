9e77d67d246771793c98c490bb36bba4ac81fd36
Rolf Wuthrich <96795973+rwuthric@users.noreply.github.com>
2025-05-20 14:55:29 +0000

    Update folder structure

--- .devcontainer/features/infra/install.sh
+++ .devcontainer/features/infra/install.sh
@@ -7,10 +7,4 @@ echo "📁 Copying infrastructure files..."
 mkdir -p "/usr/local/share/openfactory-sdk/openfactory-infra"
 cp -r "$(dirname "$0")/assets/sdk-infra/." "/usr/local/share/openfactory-sdk/openfactory-infra/"
 
-# Install Python SDK if setup.py exists
-if [ -f "$WORKSPACE_PATH/setup.py" ]; then
-    echo "📦 Installing Python SDK..."
-    pip install -e "$WORKSPACE_PATH"
-fi
-
 echo "✅ OpenFactory SDK setup complete."
